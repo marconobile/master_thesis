38524d3f7aa5b96b177a29f32300e1bb3e8f289e
marconobile <marconobile.007.mn@gmail.com>
2024-03-12 16:46:11 +0100

    Add train loop

--- test.py
+++ test.py
@@ -339,7 +339,7 @@ guacm_smiles = "/home/nobilm@usi.ch/master_thesis/guacamol/testdata.smiles"
 # valid_data = rdkit2pyg([valid_guac_mols])
 
 train_guac_mols = mols_from_file(guacm_smiles, True)
-obs = train_guac_mols[6343]
+obs = train_guac_mols[1]
 train_data = rdkit2pyg([obs])
 print(Chem.MolToSmiles(obs))
 
@@ -410,4 +410,34 @@ def memorize_batch(max_epoch, rnn, output, data_loader_, optimizer_rnn, optimize
         scheduler_output.step()
         if epoch % 500 == 0: print(f'Epoch: {epoch}/{max_epoch}, lossEdges {edge_loss:.8f}, lossNodes {node_loss:.8f}')
         epoch += 1
+
+
+VALIDATION = False
+GENERATE = True
+min_val_loss = 0
+
+while epoch <= max_epoch:
+    loss_this_epoch, loss_edg, loss_nodes = train_rnn_epoch(rnn=rnn, output=output,
+                                                            data_loader_=train_dataset_loader,
+                                                            optimizer_rnn=optimizer_rnn, optimizer_output=optimizer_output,
+                                                            node_weights=node_weights, edge_weights=edge_weights)
+    scheduler_rnn.step()
+    scheduler_output.step()
+    if epoch % 100 == 0:
+        train_log.info(f'Epoch: {epoch}/{max_epoch}, sum of Loss: {loss_this_epoch:.8f}, loss edges {loss_edg:.8f}, loss nodes {loss_nodes:.8f}')
+    if VALIDATION and epoch % 100 == 0:
+        rnn.eval()
+        output.eval()
+        val_loss, loss_edg, loss_nodes = validate_rnn_epoch(rnn, output, val_dataset_loader, node_weights, edge_weights)
+        val_log.info(f'Epoch: {epoch}/{max_epoch}, sum of Loss: {val_loss:.8f}, loss edges {loss_edg:.8f}, loss nodes {loss_nodes:.8f}')
+        if val_loss <= min_val_loss:
+            min_val_loss = val_loss
+            rnn.save(epoch)
+            output.save(epoch)
+    epoch += 1
+
+
+if GENERATE:
+    Ns = [10]#, 60000, 110000, 160000, 210000]
+    for i in Ns: generate_mols(i)
 
